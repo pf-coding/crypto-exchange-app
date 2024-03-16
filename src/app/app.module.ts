530d6bfcfa7c5f9ae70bd9c278ffb215769c7ce3
Péter Farkas <peterfarkas.sommelier@gmail.com>
2024-03-16 14:31:52 +0100

    init-update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TabsComponent } from './tabs/tabs.component';
 import { CryptoModalComponent } from './crypto-modal/crypto-modal.component';
 import { ReactiveFormsModule } from '@angular/forms';
